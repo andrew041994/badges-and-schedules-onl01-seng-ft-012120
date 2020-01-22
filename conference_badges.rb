01ccb25b2dafeef840accfb11a829d4efc880912
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 17:44:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,10 @@
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  batch_badge_message=[]
+  attendees.each { |name|batch_badge_message << "Hello, my name is #{name}."  }
 
 end
